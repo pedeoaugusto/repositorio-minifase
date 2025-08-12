8d71846ea8c9b29ce953ec7cef4ea98623760775
pedeoaugusto <pedeoaugusto2028@gmail.com>
2025-08-12 17:03:22 -0300

    s

--- monofase/Assets/scripts/boneco.cs
+++ monofase/Assets/scripts/boneco.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     
-    public float velocidade = 10;
+    public float velocidade = 40;
     public float forcaDoPulo = 4;
     
     private bool noChao = false;
