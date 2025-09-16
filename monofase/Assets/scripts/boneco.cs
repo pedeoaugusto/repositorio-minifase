ad30eced7550bb553d1381f007aafd4e5f81e3da
pedeoaugusto <pedeoaugusto2028@gmail.com>
2025-09-16 10:20:36 -0300

    I LOVE SOFHIS

--- monofase/Assets/scripts/boneco.cs
+++ monofase/Assets/scripts/boneco.cs
@@ -14,15 +14,15 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator animator;
     
-    void Start()
-    {
-        sprite = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
+       void Start()
+       {
+           sprite = GetComponent<SpriteRenderer>();
+           rb = GetComponent<Rigidbody2D>();
+           animator = GetComponent<Animator>();
+       }
 
        
-    void Update()
+   void Update()
     {
         andando = false;
         
@@ -30,6 +30,7 @@ public class Player : MonoBehaviour
         {
             gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
             sprite.flipX = true;
+
             andando = true;
         }
         
@@ -37,6 +38,7 @@ public class Player : MonoBehaviour
         {
             gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
             sprite.flipX = false;
+
             andando = true;
         }
 
@@ -45,8 +47,9 @@ public class Player : MonoBehaviour
             rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
         }
 
-        animator.SetBool("Andando",andando);
-        animator.SetBool("Pulo",!noChao);
+        //animacoes
+        animator.SetBool("Andando", andando);
+        animator.SetBool("Pulo", !noChao);
         
     }
 
@@ -58,6 +61,7 @@ public class Player : MonoBehaviour
             noChao = true;
         }
     }
+    
 
     void OnCollisionExit2D(Collision2D colisao)
     {
@@ -66,4 +70,15 @@ public class Player : MonoBehaviour
             noChao = false;
         }
     }
+
+
+    private void OnCollisionStay2D(Collision2D colisao)
+    {
+        //if (colisao.gameObject.tag == "Chao")
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = true;
+        }
+    }
+
 }
